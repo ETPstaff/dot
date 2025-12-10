a14b7090be49087f4587633aba5acb92a304af9f
ETP Staff <tpc.etp.operation@gmail.com>
2025-12-10 14:54:04 +0800

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -1,3 +1,4 @@
+$DotDir = $PSScriptRoot
 New-Item -ItemType SymbolicLink -Path (Join-Path (Join-Path (Join-Path (Join-Path $env:LOCALAPPDATA "Packages") "Microsoft.WindowsTerminal_8wekyb3d8bbwe") "LocalState") "settings.json") -Value (Join-Path (Join-Path $DotDir "wt") "settings.json") -Force | Out-Null
 New-Item -ItemType SymbolicLink -Path (Join-Path (Join-Path $env:APPDATA "helix") "config.toml") -Value (Join-Path (Join-Path $DotDir "helix") "config.toml") -Force | Out-Null
 New-Item -ItemType SymbolicLink -Path (Join-Path (Join-Path (Join-Path $env:APPDATA "Code") "User") "settings.json") -Value (Join-Path (Join-Path $DotDir "vscode") "settings.json") -Force | Out-Null
