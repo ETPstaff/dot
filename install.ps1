aa2edb2906b8aa3a3375d128cd3ebb05e11d7c36
ETP Staff <tpc.etp.operation@gmail.com>
2025-12-10 14:52:05 +0800

    Initial commit

---
+++ install.ps1
@@ -0,0 +1,4 @@
+New-Item -ItemType SymbolicLink -Path (Join-Path (Join-Path (Join-Path (Join-Path $env:LOCALAPPDATA "Packages") "Microsoft.WindowsTerminal_8wekyb3d8bbwe") "LocalState") "settings.json") -Value (Join-Path (Join-Path $DotDir "wt") "settings.json") -Force | Out-Null
+New-Item -ItemType SymbolicLink -Path (Join-Path (Join-Path $env:APPDATA "helix") "config.toml") -Value (Join-Path (Join-Path $DotDir "helix") "config.toml") -Force | Out-Null
+New-Item -ItemType SymbolicLink -Path (Join-Path (Join-Path (Join-Path $env:APPDATA "Code") "User") "settings.json") -Value (Join-Path (Join-Path $DotDir "vscode") "settings.json") -Force | Out-Null
+New-Item -ItemType SymbolicLink -Path (Join-Path (Join-Path (Join-Path $env:APPDATA "Code") "User") "keybindings.json") -Value (Join-Path (Join-Path $DotDir "vscode") "keybindings.json") -Force | Out-Null
